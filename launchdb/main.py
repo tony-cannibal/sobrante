3987b4310692a80dc16388970a5f440d65518bbc
tony-cannibal <castrovital.luis@gmail.com>
2025-02-07 16:46:21 -0600

    minor update

--- launchdb/main.py
+++ launchdb/main.py
@@ -2,11 +2,11 @@ import mariadb
 
 
 database = {
-    "host": "172.18.4.58",
-    "database": "sobrantes",
     "user": "yura_admin",
     "password": "Metallica24+",
+    "host": "172.18.4.58",
     "port": 3306,
+    "database": "sobrante",
 }
 
 
@@ -16,57 +16,90 @@ def createDatabase(db):
     cur.execute("CREATE DATABASE sobrante")
 
 
-def createTable(db):
+def createTables(**db):
     con = mariadb.connect(**db)
     cur = con.cursor()
     cur.execute(
         """
-            CREATE TABLE sobrante_m1 (
-                id INT AUTO_INCREMENT PRIMARY KEY, 
-                codigo VARCHAR,
-                num_parte VARCHAR,
-                circuito VARCHAR,
-                cable VARCHAR,
-                terminal_l VARCHAR,
-                terminal_r VARCHAR,
-                sello_l VARCHAR,
-                sello_r VARCHAR,
-                longitud VARCHAR,
-                cantidad INT,
-                fecha DEFAULT CURRENT_TIMESTAMP,
-                restante INT,
-                actualizado ON UPDATE CURRENT_TIMESTAMP
-            )
-        """
+		CREATE TABLE IF NOT EXISTS sobrante_m1 (
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        lote VARCHAR(12),
+		num_parte VARCHAR(30),
+		circuito VARCHAR(30) NOT NULL,
+		cable VARCHAR(10),
+		terminal_l VARCHAR(14),
+		terminal_r VARCHAR(14),
+        sello_l VARCHAR(14),
+        sello_r VARCHAR(14),
+        longitud INT,
+        cantidad INT,
+        fecha DEFAULT CURRENT_TIMESTAMP,
+        restante INT,
+        actualizado ON UPDATE CURRENT_TIMESTAMP
+		);"""
     )
-    cur.execute(
-        """
-            CREATE TABLE sobrante_m2 (
-                id INT AUTO_INCREMENT PRIMARY KEY, 
-                codigo VARCHAR,
-                num_parte VARCHAR,
-                circuito VARCHAR,
-                cable VARCHAR,
-                terminal_l VARCHAR,
-                terminal_r VARCHAR,
-                sello_l VARCHAR,
-                sello_r VARCHAR,
-                longitud VARCHAR,
-                cantidad INT,
-                fecha DEFAULT CURRENT_TIMESTAMP,
-                restante INT,
-                actualizado ON UPDATE CURRENT_TIMESTAMP
-            )
-        """
-    )
-    cur.execute(
-        """
-            CREATE TABLE movimientos_m1 (
-                id INT AUTO_INCREMENT PRIMARY KEY,
+    # """
+    #     CREATE TABLE sobrante_m1 (
+    #         id INT AUTO_INCREMENT PRIMARY KEY,
+    #         codigo VARCHAR,
+    #         num_parte VARCHAR,
+    #             circuito VARCHAR,
+    #             cable VARCHAR,
+    #             terminal_l VARCHAR,
+    #             terminal_r VARCHAR,
+    #             sello_l VARCHAR,
+    #             sello_r VARCHAR,
+    #             longitud VARCHAR,
+    #             cantidad INT,
+    #             fecha DEFAULT CURRENT_TIMESTAMP,
+    #             restante INT,
+    #             actualizado ON UPDATE CURRENT_TIMESTAMP
+    #         );
+    #     """
+    # )
+    # cur.execute(
+    #     """
+    #         CREATE TABLE sobrante_m2 (
+    #             id INT AUTO_INCREMENT PRIMARY KEY,
+    #             codigo VARCHAR,
+    #             num_parte VARCHAR,
+    #             circuito VARCHAR,
+    #             cable VARCHAR,
+    #             terminal_l VARCHAR,
+    #             terminal_r VARCHAR,
+    #             sello_l VARCHAR,
+    #             sello_r VARCHAR,
+    #             longitud VARCHAR,
+    #             cantidad INT,
+    #             fecha DEFAULT CURRENT_TIMESTAMP,
+    #             restante INT,
+    #             actualizado ON UPDATE CURRENT_TIMESTAMP
+    #         );
+    #     """
+    # )
+    # cur.execute(
+    #     """
+    #         CREATE TABLE movimientos_m1 (
+    #             id INT AUTO_INCREMENT PRIMARY KEY,
+    #             ref INT,
+    #             np VARCHAR,
+    #             circuito VARCHAR,
+    #             fecha DEFAULT CURRENT_TIMESTAMP
+    #             );
+    #     """
+    # )
+    # cur.execute(
+    #     """
+    #         CREATE TABLE movimientos_m1 (
+    #             id INT AUTO_INCREMENT PRIMARY KEY,
+    #             ref INT,
+    #             np VARCHAR,
+    #             circuito VARCHAR,
+    #             fecha DEFAULT CURRENT_TIMESTAMP
+    #             );
+    #     """
+    # )
 
-                np VARCHAR,
-                circuito VARCHAR,
-                fecha DEFAULT CURRENT_TIMESTAMP
-                )
-        """
-    )
+
+if __name__ == "__main__":
+    createTables(**database)
